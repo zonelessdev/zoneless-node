439d3e0d6d7a4c56b98f1d33035e7df5c2c7c029
Ben Stokes <benstokes@Bens-Air.lan>
2026-01-22 17:42:04 +0000

    fix: Remove MainWallet export

--- src/types/index.ts
+++ src/types/index.ts
@@ -11,7 +11,6 @@ export * from './BalanceTransaction';
 export * from './Event';
 export * from './ExternalWallet';
 export * from './IdempotencyKey';
-export * from './MainWallet';
 export * from './Payout';
 export * from './Person';
 export * from './QueryParameters';
